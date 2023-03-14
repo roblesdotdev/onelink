670df6ae11e20be5167e23f4de12678251bbb362
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-03-14 11:10:45 -0300

    Listing link (#2)

--- app/routes/admin/links.tsx
+++ app/routes/admin/links.tsx
@@ -1,10 +1,27 @@
-import type { ActionFunction } from '@remix-run/node'
+import type { ActionFunction, LoaderFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { useFetcher } from '@remix-run/react'
+import { useFetcher, useLoaderData, useSubmit } from '@remix-run/react'
+import { useEffect, useRef } from 'react'
 import invariant from 'tiny-invariant'
-import { createLinks } from '~/utils/link.server'
-import { requireSessionUser } from '~/utils/session.server'
+import {
+  createLinks,
+  deleteLink,
+  getUserLinks,
+  toggleLinkVisibility,
+} from '~/utils/link.server'
+import { requireSessionUser, requireUser } from '~/utils/session.server'
 import { validateTitle, validateUrl } from '~/utils/validation'
+
+type LoaderData = {
+  links: Awaited<ReturnType<typeof getUserLinks>>
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await requireUser(request)
+  const links = await getUserLinks(user.id)
+
+  return json<LoaderData>({ links })
+}
 
 type ActionData = {
   status: 'success' | 'error'
@@ -18,20 +35,32 @@ type ActionData = {
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
   const userId = await requireSessionUser(request)
-  const { url, title } = Object.fromEntries(formData)
-  invariant(typeof url === 'string', 'url type is invalid')
-  invariant(typeof title === 'string', 'title type is invalid')
-
-  const errors = {
-    url: validateUrl(url),
-    title: validateTitle(title),
+  const { action, linkId, url, title, published } = Object.fromEntries(formData)
+  invariant(typeof action === 'string', 'action type is invalid')
+  if (action === 'delete') {
+    invariant(typeof linkId === 'string', 'linkId type is invalid')
+    await deleteLink(linkId)
+  }
+  if (action === 'toggle') {
+    invariant(typeof linkId === 'string', 'linkId type is invalid')
+    await toggleLinkVisibility({ id: linkId, published: published === 'on' })
   }
 
-  if (Object.values(errors).some(Boolean)) {
-    return json<ActionData>({ status: 'error', errors }, { status: 400 })
-  }
+  if (action === 'create') {
+    invariant(typeof url === 'string', 'url type is invalid')
+    invariant(typeof title === 'string', 'title type is invalid')
 
-  await createLinks({ url, title, userId })
+    const errors = {
+      url: validateUrl(url),
+      title: validateTitle(title),
+    }
+
+    if (Object.values(errors).some(Boolean)) {
+      return json<ActionData>({ status: 'error', errors }, { status: 400 })
+    }
+
+    await createLinks({ url, title, userId })
+  }
 
   return json({})
 }
@@ -39,6 +68,14 @@ export const action: ActionFunction = async ({ request }) => {
 export default function Links() {
   const fetcher = useFetcher<ActionData>()
   const errors = fetcher.data?.errors
+  const createFormRef = useRef<HTMLFormElement>(null)
+  const { links } = useLoaderData<LoaderData>()
+  const submit = useSubmit()
+
+  useEffect(() => {
+    if (!createFormRef) return
+    if (fetcher.state !== 'idle') createFormRef.current?.reset()
+  }, [fetcher])
 
   return (
     <div>
@@ -49,6 +86,7 @@ export default function Links() {
         autoCapitalize="off"
         autoComplete="off"
       >
+        <input type="hidden" name="action" defaultValue="create" />
         <div className="flex flex-col">
           <label htmlFor="url">Url</label>
           <input
@@ -97,6 +135,38 @@ export default function Links() {
           </button>
         </div>
       </fetcher.Form>
+
+      <div>
+        {links.map(link => (
+          <div key={link.id} className="rounded-md bg-white p-4 shadow">
+            <div className="flex items-start justify-between gap-4">
+              <div>
+                <h1 className="font-bold">{link.title}</h1>
+                <h2>{link.url}</h2>
+              </div>
+              <fetcher.Form
+                className="px-4 py-1"
+                method="post"
+                noValidate
+                onChange={e => submit(e.currentTarget, { replace: true })}
+              >
+                <input type="hidden" name="action" defaultValue="toggle" />
+                <input type="hidden" name="linkId" defaultValue={link.id} />
+                <input
+                  type="checkbox"
+                  defaultChecked={link.published}
+                  name="published"
+                />
+              </fetcher.Form>
+            </div>
+            <fetcher.Form className="mt-2 flex" method="post" noValidate>
+              <input type="hidden" name="action" defaultValue="delete" />
+              <input type="hidden" name="linkId" defaultValue={link.id} />
+              <button type="submit">Delete</button>
+            </fetcher.Form>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
