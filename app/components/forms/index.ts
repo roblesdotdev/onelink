5af1ccbbeba290fee322da581627f21aaea6b19b
roblesdotdev <httpsnip@gmial.com>
2023-03-20 12:03:45 -0300

    dash mobile

--- app/components/forms/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as DeleteForm } from './delete-form'
-export { default as ToggleForm } from './toggle-form'
--- app/routes/admin.links.tsx
+++ app/routes/admin.links.tsx
@@ -1,16 +1,17 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
+import * as Switch from '@radix-ui/react-switch'
 import type { ActionFunction, LoaderFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { Link, Outlet, useLoaderData } from '@remix-run/react'
+import { Form, Link, Outlet, useLoaderData, useSubmit } from '@remix-run/react'
+import { useState } from 'react'
 import invariant from 'tiny-invariant'
-import { DeleteForm, ToggleForm } from '~/components/forms'
+import { GridIcon, PlusIcon, ThrashIcon } from '~/components/icons'
 import {
-  createLinks,
   deleteLink,
   getUserLinks,
   toggleLinkVisibility,
 } from '~/utils/link.server'
-import { requireSessionUser, requireUser } from '~/utils/session.server'
-import { validateTitle, validateUrl } from '~/utils/validation'
+import { requireUser } from '~/utils/session.server'
 
 type LoaderData = {
   links: Awaited<ReturnType<typeof getUserLinks>>
@@ -34,8 +35,7 @@ export type LinksActionData = {
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
-  const userId = await requireSessionUser(request)
-  const { action, linkId, url, title, published } = Object.fromEntries(formData)
+  const { action, linkId, published } = Object.fromEntries(formData)
   invariant(typeof action === 'string', 'action type is invalid')
   if (action === 'delete') {
     invariant(typeof linkId === 'string', 'linkId type is invalid')
@@ -47,22 +47,6 @@ export const action: ActionFunction = async ({ request }) => {
     await toggleLinkVisibility({ id: linkId, published: published === 'on' })
   }
 
-  if (action === 'create') {
-    invariant(typeof url === 'string', 'url type is invalid')
-    invariant(typeof title === 'string', 'title type is invalid')
-
-    const errors = {
-      url: validateUrl(url),
-      title: validateTitle(title),
-    }
-
-    if (Object.values(errors).some(Boolean)) {
-      return json<LinksActionData>({ status: 'error', errors }, { status: 400 })
-    }
-
-    await createLinks({ url, title, userId })
-  }
-
   return json<LinksActionData>({ status: 'success' })
 }
 
@@ -72,21 +56,103 @@ export default function Links() {
   return (
     <div>
       <Outlet />
-      <Link to="new">Create</Link>
-      <div>
+      <div className="flex items-center py-4">
+        <Link
+          className="flex w-full items-center justify-center gap-2 rounded-md bg-slate-900 py-4 text-white"
+          to="new"
+        >
+          <span>Create a new link</span>
+          <PlusIcon />
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">
         {links.map(link => (
-          <div key={link.id} className="rounded-md bg-white p-4 shadow">
-            <div className="flex items-start justify-between gap-4">
-              <div>
-                <h1 className="font-bold">{link.title}</h1>
-                <h2>{link.url}</h2>
+          <div key={link.id} className="rounded-md bg-white py-4 px-2 shadow">
+            <div className="flex gap-4">
+              <div className="p-2 text-gray-400/60">
+                <GridIcon />
               </div>
-              <ToggleForm published={link.published} linkId={link.id} />
+              <div className="flex flex-1 flex-col">
+                <div className="flex w-full items-start justify-between gap-4">
+                  <div>
+                    <h1 className="font-bold">{link.title}</h1>
+                    <h2>{link.url}</h2>
+                  </div>
+                  <ToggleForm published={link.published} linkId={link.id} />
+                </div>
+                <DeleteForm linkId={link.id} />
+              </div>
             </div>
-            <DeleteForm linkId={link.id} />
           </div>
         ))}
       </div>
     </div>
+  )
+}
+
+function DeleteForm({ linkId }: { linkId: string }) {
+  const [open, setOpen] = useState(false)
+  return (
+    <Form className="mt-3 flex" method="post" noValidate>
+      <input type="hidden" name="action" defaultValue="delete" />
+      <input type="hidden" name="linkId" defaultValue={linkId} />
+
+      <Collapsible.Root open={open} onOpenChange={() => setOpen(!open)}>
+        <div className="flex items-center justify-between">
+          <Collapsible.Trigger asChild>
+            <button className="flex items-center gap-2 rounded-md bg-slate-100 p-2">
+              <ThrashIcon className="h-4 w-4" />
+            </button>
+          </Collapsible.Trigger>
+        </div>
+
+        <Collapsible.Content>
+          <div className="mt-4 flex items-center gap-4">
+            <button
+              type="submit"
+              className="rounded-md bg-red-600 py-2 px-4 text-white"
+            >
+              Confirm
+            </button>
+            <button
+              type="button"
+              className="underline"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </Collapsible.Content>
+      </Collapsible.Root>
+    </Form>
+  )
+}
+
+function ToggleForm({
+  published,
+  linkId,
+}: {
+  published: boolean
+  linkId: string
+}) {
+  const submit = useSubmit()
+  return (
+    <Form
+      className="px-4 py-1"
+      method="post"
+      noValidate
+      onChange={e => submit(e.currentTarget, { replace: true })}
+    >
+      <input type="hidden" name="action" defaultValue="toggle" />
+      <input type="hidden" name="linkId" defaultValue={linkId} />
+      <Switch.Root
+        id="airplane-mode"
+        defaultChecked={published}
+        name="published"
+        className="relative h-6 w-10 rounded-full bg-gray-400/60 data-[state=checked]:bg-slate-900"
+      >
+        <Switch.Thumb className="block h-5 w-5 translate-x-[2px] rounded-full bg-white transition data-[state=checked]:translate-x-[18px]" />
+      </Switch.Root>
+    </Form>
   )
 }
